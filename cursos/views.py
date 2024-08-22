45881a32204f59c2de84fef3ba8529f8eb03a453
Samylly Pires <samyllyh@gmail.com>
2024-08-22 11:38:23 -0300

    2 versao usando genericView para implementacao dos verbos HTTP

--- cursos/views.py
+++ cursos/views.py
@@ -1,36 +1,26 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
-class CursoAPIView(APIView):
-    def get(self, request):
-        cursos = Curso.objects.all()
-        serializer = CursoSerializer(cursos, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = CursoSerializer(data=request.data)
-        serializer.is_valid(raise_exception = True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-        """
-        OUTROS TIPOS DE RETORNOS
+class CursosAPIView(generics.ListCreateAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-        return Response({"msg": "Criado com sucesso"}, status=status.HTTP_201_CREATED)
-        return Response({"id": serializer.data['id'], "curso": serializerdata['titulo']}, status=status.HTTP_201_CREATED)
-        """
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-class AvaliacaoAPIView(APIView):
-    def get(self, request):
-        avaliacoes = Avaliacao.objects.all()
-        serializer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = AvaliacaoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+"""
+Quando usamos o generic para os verbos HTTP:
+    usamos ListCreateAPIView para GET e POST (endpoint de colecao)
+    usamos RetriveUpdateDestroyAPIView para os demias metodo(endpoint individuais)
+"""
